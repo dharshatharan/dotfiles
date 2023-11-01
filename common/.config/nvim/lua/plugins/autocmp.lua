85ffb580be0370e92db721d8c9f9f95f6c7f68cb
dharshatharan <dharshatharan@gmail.com>
2023-11-01 10:42:55 -0400

    Change tab remap for copilot

--- common/.config/nvim/lua/plugins/autocmp.lua
+++ common/.config/nvim/lua/plugins/autocmp.lua
@@ -65,5 +65,9 @@ return {
   },
   {
     "github/copilot.vim",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<leader><tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end
   },
 }
