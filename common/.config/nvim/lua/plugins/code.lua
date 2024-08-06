b52e35222966b42d4784575a28e7d59306fa7a77
dharshatharan <dharshatharan@gmail.com>
2024-08-06 09:40:10 -0400

    Add textobj entire

--- common/.config/nvim/lua/plugins/code.lua
+++ common/.config/nvim/lua/plugins/code.lua
@@ -1,4 +1,9 @@
 return {
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
+
+  {
+    "kana/vim-textobj-entire",
+    dependencies = { "kana/vim-textobj-user" },
+  },
 }
