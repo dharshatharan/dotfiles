130307b89c41d16159727e604146fda8ca1cba2d
dharshatharan <dharshatharan@gmail.com>
2024-08-10 00:08:15 -0400

    both: install octo

--- common/.config/nvim/lua/plugins/git.lua
+++ common/.config/nvim/lua/plugins/git.lua
@@ -26,4 +26,17 @@ return {
       end,
     },
   },
+
+  {
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      -- OR 'ibhagwan/fzf-lua',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require "octo".setup()
+    end
+  }
 }
