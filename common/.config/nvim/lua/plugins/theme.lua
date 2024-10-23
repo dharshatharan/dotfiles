68f83fd5f651c6fa9110cd478eb0e8a75e83474f
dharshatharan <dharshatharan@gmail.com>
2024-10-23 07:35:46 -0400

    both: nvim remove transparent bg

--- common/.config/nvim/lua/plugins/theme.lua
+++ common/.config/nvim/lua/plugins/theme.lua
@@ -5,12 +5,12 @@ return {
     config = function()
       vim.cmd.colorscheme 'rose-pine'
 
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-      vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
-      vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+      -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+      -- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+      -- vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+      -- vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
     end,
   },
 }
