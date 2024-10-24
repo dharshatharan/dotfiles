eaa142624a3105fa9acb03afeda29caee8be8e6c
dharshatharan <dharshatharan@gmail.com>
2024-10-24 08:49:58 -0400

    both: move lazygit to git.lua

--- common/.config/nvim/lua/plugins/tools.lua
+++ common/.config/nvim/lua/plugins/tools.lua
@@ -17,11 +17,4 @@ return {
   },
   'tpope/vim-surround',
   'wakatime/vim-wakatime',
-  {
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
 }
