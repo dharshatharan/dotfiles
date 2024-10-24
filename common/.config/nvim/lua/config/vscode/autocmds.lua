d0d1b8a4d115ff93eb825e11e3907a5b9729119d
dharshatharan <dharshatharan@gmail.com>
2024-10-24 08:49:58 -0400

    both: move lazygit to git.lua

---
+++ common/.config/nvim/lua/config/vscode/autocmds.lua
@@ -0,0 +1,21 @@
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- Set default indentation to 2 spaces for JavaScript and TypeScript
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
