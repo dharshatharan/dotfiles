694ca231dfa63c4dd251859efa188906b3dfd57c
dharshatharan <dharshatharan@gmail.com>
2025-07-24 22:21:50 -0400

    both: git reset hunk

--- common/.config/nvim/lua/plugins/git.lua
+++ common/.config/nvim/lua/plugins/git.lua
@@ -23,6 +23,7 @@ return {
           { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
         vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
         vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+        vim.keymap.set('n', '<leader>rh', require('gitsigns').reset_hunk, { buffer = bufnr, desc = '[R]eset [H]unk' })
       end,
     },
   },
